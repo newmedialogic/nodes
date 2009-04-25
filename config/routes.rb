f6c7ca3895a876037287870bd592fd2391d54af1
Aaron Longwell <aaron@newmedialogic.com>
2009-04-25 14:23:24 -0700

    Added nodes route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
 
+  map.resources :nodes
+    # We require that the :id parameter for this resource
+    # begin with an integer. This allows the SEO route (see bottom)
+    # to intercept routes like /nodes/some_node_title
+    #
+    :requirements => { :id => /\d.*/ }
+  
   map.seo '*path', :controller => 'nodes', :action => 'show'
 
 end
