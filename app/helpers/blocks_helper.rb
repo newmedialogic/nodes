714e210119653ddc55f25d9eff4bbfadda4e64e6
Aaron Longwell <aaron@newmedialogic.com>
2009-04-28 23:03:21 -0700

    Minor revisions.

--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -1,9 +1,28 @@
 module BlocksHelper
 
-  def render_blocks_for(name)
+  BLOCKS = {
+    :content_top => ['/mission_top'],
+    :inner_sidebar => ['/pio', '/principles']
+  }
+
+  def has_blocks_for?(name)
     if params[:path].empty?
-      render "/top_story"
+      if BLOCKS.has_key?(name)
+        return true
+      end
     end
+  end
+
+
+  def render_blocks_for(name)
+    val = ""
+    if params[:path].empty?
+      return if BLOCKS[name].empty?
+      BLOCKS[name].each do |block|
+        val = val + render("#{block}")
+      end
+    end
+    val
   end
 
 end
