138b10f46759e850f0ed980d0e431a1472af3dda
Aaron Longwell <aaron@newmedialogic.com>
2009-05-14 15:55:27 -0700

    Fixed deprecated use of type method.

--- lib/nodes.rb
+++ lib/nodes.rb
@@ -35,7 +35,7 @@ module Nodes
     # for a class called BlogPost.
     #
     def provides_nodes(options = {})
-      Nodes.node_classes << self.type.name unless Nodes.node_classes.include?(self.type.name)
+      Nodes.node_classes << self.class.name unless Nodes.node_classes.include?(self.type.name)
     end
 
   end
