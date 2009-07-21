385b04726bb61b871058c4382d42d3990f606617
Aaron Longwell <aaron@newmedialogic.com>
2009-07-21 14:09:53 -0700

    Preparing for move from a gem to a regular plugin (temporarily).

--- app/helpers/node_comments_helper.rb
+++ app/helpers/node_comments_helper.rb
@@ -1,6 +1,22 @@
 module NodeCommentsHelper
 
   def comments_and_form_for(node, options = {})
+    capture do
+      comments_for(node, options)
+      if allow_posting_node_comments
+        comment_form_for(node, options)
+      end
+    end
+  end
+
+
+  def comments_for(node, options = {})
+    render :partial => "/node_comments/comments_for", :locals => { :node => node }
+  end
+
+
+  def comment_form_for(node, options = {})
+    render :partial => "/node_comments/inline_comment_form", :locals => { :node => node }
   end
 
 end
