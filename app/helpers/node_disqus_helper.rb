9b15c9332953df17ec9a4b3af13b533c7fc83c57
Aaron Longwell <aaron@newmedialogic.com>
2009-09-29 14:08:43 -0700

    Added developer flag for working in developer mode.

--- app/helpers/node_disqus_helper.rb
+++ app/helpers/node_disqus_helper.rb
@@ -3,8 +3,10 @@ module NodeDisqusHelper
   def disqus_javascript(site_name)
     html = <<EOF
       <script type="text/javascript">
+        var disqus_developer = #{ENV['RAILS_ENV'] == 'production' ? 0 : 1};
       //<![CDATA[
       (function() {
+
         var links = document.getElementsByTagName('a');
         var query = '?';
         for(var i = 0; i < links.length; i++) {
@@ -19,7 +21,7 @@ module NodeDisqusHelper
 EOF
   end
 
-  def disqus_placeholder
+  def disqus_comments
     '<div id="disqus_thread"></div><script type="text/javascript" src="http://disqus.com/forums/childrensjusticealliance/embed.js"></script><noscript><a href="http://childrensjusticealliance.disqus.com/?url=ref">View the discussion thread.</a></noscript><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>'
   end
 
